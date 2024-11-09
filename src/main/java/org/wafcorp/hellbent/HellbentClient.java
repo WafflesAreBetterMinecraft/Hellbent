c6b3494413496a0d31183d11a470e3d61c52b75f
WafflesAreBetter <wafflesarebetter@wafcorp.org>
2024-11-08 20:15:53 -0700

    Initial commit

---
+++ src/main/java/org/wafcorp/hellbent/HellbentClient.java
@@ -0,0 +1,10 @@
+package org.wafcorp.hellbent;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HellbentClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
